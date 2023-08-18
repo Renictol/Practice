287db5ca0e2147872a86fe429d4bcecbc1b3e5a5
Morenike <iswcorp@ISW-TECH-AO2.Interswitchng.com>
2023-08-18 16:09:35 +0100

    Added a new branch

--- src/main/java/com/interswitch/maven/App.java
+++ src/main/java/com/interswitch/maven/App.java
@@ -2,20 +2,32 @@ package com.interswitch.maven;
 
 import com.interswitch.maven.model.Calculator;
 
+import java.time.LocalDateTime;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main ( String[] args )
 
     {
-        Calculator calculator = new Calculator();
-        calculator.addNumbers();
-        System.out.println();
-        calculator.subtractNumbers();
-        System.out.println();
-        calculator.multipyNumbers();
+        System.out.println("New Go-home feature by " + "Backbone Team suggested at " +
+                LocalDateTime.now());
+//        Calculator calculator = new Calculator();
+//        calculator.addNumbers();
+//        System.out.println();
+//        calculator.subtractNumbers();
+//        System.out.println();
+//        calculator.multipyNumbers();
+
+        UpperLowerCase ulcase = new UpperLowerCase();
+        System.out.printf("%s to %s\n", "hello world", ulcase.wordToUpperCase("hello world"));
+
+        System.out.printf("%s to %s\n", "HELLO WORLD", ulcase.wordToLowerCase("HELLO WORLD"));
+
+
     }
 }
